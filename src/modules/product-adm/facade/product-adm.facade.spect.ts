49be7821aaa015d6e74183237803ae2f5893968d
Henrique Lisboa Brugnarotto <henriquebrug@outlook.com>
2023-10-10 16:00:47 -0300

    Implementando Factory para nossa Facade

--- src/modules/product-adm/facade/product-adm.facade.spect.ts
+++ src/modules/product-adm/facade/product-adm.facade.spect.ts
@@ -3,6 +3,7 @@ import { Sequelize } from "sequelize-typescript";
 import ProductRepository from "../repository/product.repository";
 import AddProductUseCase from "../usecase/add-product/add-product.usecase";
 import ProductAdmFacade from "./product-adm.facade";
+import ProductAdmFacadeFactory from '../factory/facade.factory';
 
 describe("ProductAdmFacade test", () => {
     let sequelize: Sequelize;
@@ -22,12 +23,14 @@ describe("ProductAdmFacade test", () => {
     });
 
     it("should create a product", async () => {
-        const productRepository = new ProductRepository();
-        const addProductUseCase = new AddProductUseCase(productRepository);
-        const productFacade = new ProductAdmFacade({
-            addUseCase: addProductUseCase,
-            stockUseCase: undefined,
-        });
+        // const productRepository = new ProductRepository();
+        // const addProductUseCase = new AddProductUseCase(productRepository);
+        // const productFacade = new ProductAdmFacade({
+        //     addUseCase: addProductUseCase,
+        //     stockUseCase: undefined,
+        // });
+
+        const productFacade = ProductAdmFacadeFactory.create();
 
         const input = {
             id: "1",
