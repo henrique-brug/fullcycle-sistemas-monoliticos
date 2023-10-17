a02eea71f852e2f6eb49792198cfd3b78ec9821d
Henrique Lisboa Brugnarotto <henriquebrug@outlook.com>
2023-10-17 16:35:07 -0300

    Finalizando FindProductUseCase

--- src/modules/store-catalog/repository/product.repository.ts
+++ src/modules/store-catalog/repository/product.repository.ts
@@ -1,7 +1,7 @@
 
 import Id from "../../@shared/domain/value-object/id.value-object";
 import Product from "../domain/product.entity";
-import ProductGateway from "../gateway/pruduct.gateway";
+import ProductGateway from "../gateway/product.gateway";
 import ProductModel from "./product.model";
 
 export default class ProductRepository implements ProductGateway {
