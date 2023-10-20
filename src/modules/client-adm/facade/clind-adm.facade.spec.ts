6866b6af7d8f91a3354ccb7885f270ee676bcc2b
Henrique Lisboa Brugnarotto <henriquebrug@outlook.com>
2023-10-20 07:40:40 -0300

    Finalizando Facade e Factory

--- src/modules/client-adm/facade/clind-adm.facade.spec.ts
+++ src/modules/client-adm/facade/clind-adm.facade.spec.ts
@@ -1,7 +1,9 @@
 import { Sequelize } from "sequelize-typescript";
+import ClientAdmFacadeFactory from "../factory/client-adm.facade.factory";
 import { ClientModel } from "../repository/client.model";
 import ClientRepository from "../repository/client.repository";
 import AddClientUseCase from "../usecase/add-client/add-client.usecase";
+import FindClientUseCase from "../usecase/find-client/find-client.usecase";
 import ClientAdmFacade from "./clind-adm.facade";
 
 describe("ClientAdmFacade test", () => {
@@ -41,6 +43,34 @@ describe("ClientAdmFacade test", () => {
         const client = await ClientModel.findOne({ where: {id:"1"}});
 
         expect(client).toBeDefined();
+        expect(client.name).toEqual(input.name);
+        expect(client.email).toEqual(input.email);
+        expect(client.address).toEqual(input.address);
+    });
+
+    it("should find a client", async () => {
+        // const repository = new ClientRepository();
+        // const addUsecase = new AddClientUseCase(repository);
+        // const findUsecase = new FindClientUseCase(repository);
+        // const facade = new ClientAdmFacade({
+        //     addUsecase: addUsecase,
+        //     findUsecase: findUsecase,
+        // });
+
+        const facade = ClientAdmFacadeFactory.create();
+
+        const input = {
+            id: "1",
+            name: "Client 1",
+            email: "a@a.com",
+            address: "Address 1",
+        }
+        await facade.add(input);
+
+        const client = await facade.find({ id:"1"});
+
+        expect(client).toBeDefined();
+        expect(client.id).toEqual(input.id);
         expect(client.name).toEqual(input.name);
         expect(client.email).toEqual(input.email);
         expect(client.address).toEqual(input.address);
